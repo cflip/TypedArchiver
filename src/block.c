36b0239a798979ae02ff4775941b3ead4b57a5d7
Cflip <36554078+cflip@users.noreply.github.com>
2020-12-16 09:31:53 -0700

    Individually write variables to buffer

--- src/block.c
+++ src/block.c
@@ -4,6 +4,12 @@
 #include <string.h>
 
 void write_block(void* buffer, struct block_header* header, void* data) {
-	memcpy(buffer, header, sizeof(struct block_header));
-	memcpy((char*) buffer + sizeof(struct block_header), data, header->size);
+	char* p = buffer;
+
+	memcpy(p,      &header->name, NAME_LENGTH);
+	memcpy(p + 16, &header->type, sizeof(uint8_t));
+	memcpy(p + 17, &header->size, sizeof(uint32_t));
+	memcpy(p + 21, &header->date, sizeof(uint32_t));
+
+	memcpy(p + 25, data, header->size);
 }
